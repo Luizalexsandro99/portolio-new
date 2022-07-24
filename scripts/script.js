b734dc591d1feed33a577719c206a8c3924c4afc
Luiz Alexsandro <100519613+Luizalexsandro99@users.noreply.github.com>
2022-07-24 20:17:09 -0300

    Atualization full

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,5 @@
-console.log('oi')
+let butaoGit = document.querySelectorAll('.button')[1]
+let linkProjetos = document.querySelector("#linkMaisProjetos")
+linkProjetos.onclick = () =>{
+    butaoGit.classList.add('aumentando')
+}
